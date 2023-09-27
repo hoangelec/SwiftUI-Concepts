22d44dbffbd394d9a515152562771a3bde7fa17a
Mike Cap <hoangelec@icloud.com>
2023-09-27 11:35:11 +0700

    Added Choosing the right way to hide view

--- layering-content/CaptionedPhoto/CaptionedPhoto.swift
+++ layering-content/CaptionedPhoto/CaptionedPhoto.swift
@@ -1,6 +1,6 @@
 /*
-See LICENSE folder for this sample’s licensing information.
-*/
+ See LICENSE folder for this sample’s licensing information.
+ */
 
 import SwiftUI
 
@@ -8,14 +8,21 @@ struct CaptionedPhoto: View {
     let assetName: String
     let captionText: String
     var body: some View {
-        Image(assetName)
-            .resizable()
-            .scaledToFit()
-            .overlay(alignment: .bottom) {
-                Caption(text: captionText)
-            }
-            .clipShape(RoundedRectangle(cornerRadius: 10.0, style: .continuous))
-            .padding()
+        
+        List {
+            Image(assetName)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+//                .frame(width: 100, height: 100)
+
+//                .aspectRatio(contentMode: .fill)
+//                .frame(width: 300, height: 200)
+        }
+        
+        
+//            .resizable()
+//            .scaledToFit()
+//
     }
 }
 
@@ -36,9 +43,12 @@ struct CaptionedPhoto_Previews: PreviewProvider {
     static let portraitName = "Portrait"
     static let portraitCaption = "This photo is taller than it is wide."
     static var previews: some View {
-        CaptionedPhoto(assetName: portraitName,
-                       captionText: portraitCaption)
-        CaptionedPhoto(assetName: landscapeName,
-                       captionText: landscapeCaption)
+        Group {
+            CaptionedPhoto(assetName: portraitName,
+                           captionText: portraitCaption)
+            CaptionedPhoto(assetName: landscapeName,
+                           captionText: landscapeCaption)
+        }
+        
     }
 }
