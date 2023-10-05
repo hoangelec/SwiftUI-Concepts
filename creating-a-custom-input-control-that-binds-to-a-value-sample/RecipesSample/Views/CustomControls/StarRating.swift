f2ae6f9ec2bb1a2fe90b19de7f254579422cc97c
Mike Cap <hoangelec@icloud.com>
2023-10-05 14:28:19 +0700

    Finished binding

--- creating-a-custom-input-control-that-binds-to-a-value-sample/RecipesSample/Views/CustomControls/StarRating.swift
+++ creating-a-custom-input-control-that-binds-to-a-value-sample/RecipesSample/Views/CustomControls/StarRating.swift
@@ -21,3 +21,27 @@ struct StarRating: View {
         }
     }
 }
+
+struct StarRating_Previews: PreviewProvider {
+
+    public struct StatefulPreviewWrapper<Value, Content: View>: View {
+        @State var value: Value
+        var content: (Binding<Value>) -> Content
+
+        public var body: some View {
+            content($value)
+        }
+
+        public init(_ value: Value, content: @escaping (Binding<Value>) -> Content) {
+            self._value = State(wrappedValue: value)
+            self.content = content
+        }
+    }
+
+    static var previews: some View {
+        StatefulPreviewWrapper.init(2) {
+            StarRating(rating: $0)
+        }
+    }
+}
+
